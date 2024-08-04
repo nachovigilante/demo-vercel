1e7179307204dcc70327a4a2631470424b8cd242
nachovigilante <nachovigilante@gmail.com>
2024-08-04 14:49:29 -0300

    Arreglos menores

--- dbconfig.js
+++ dbconfig.js
@@ -1,13 +1,10 @@
+import "dotenv/config";
 
-// export POSTGRES_HOST=ep-blue-feather-a4ve7rp2.us-east-1.aws.neon 
-// export POSTGRES_USER=default
-// export POSTGRES_PASSWORD=xxxxx
-// 
 export const config = {
-    host :process.env.POSTGRES_HOST,
-    database:"ort-prueba",
-    user:process.env.POSTGRES_USER,
-    password:process.env.POSTGRES_PASSWORD,
-    port:5432,
-    ssl: true
-}
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DATABASE,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    port: 5432,
+    ssl: true,
+};
